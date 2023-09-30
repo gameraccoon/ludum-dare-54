6e74830ebdffdbd0ee158ec5fb3a90beeae53038
Pavel Grebnev <ruwhynot@gmail.com>
2023-09-30 18:33:47 +0200

    Fix wall collision not being rotated

--- scripts/Wall.gd
+++ scripts/Wall.gd
@@ -10,6 +10,7 @@ func set_wall_type(is_vertical: bool):
 	if is_vertical:
 		region_rect.size.x = Consts.WallWidth
 		region_rect.size.y = Consts.WallLength
+		$Area2D.rotate(PI * 0.5)
 	else:
 		region_rect.size.x = Consts.WallLength
 		region_rect.size.y = Consts.WallWidth
