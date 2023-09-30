892910934f02315d2e468bb7a5d1fa3f0609a141
Pavel Grebnev <ruwhynot@gmail.com>
2023-09-30 13:55:22 +0200

    Player is moving randomly by one of the axis

--- Main.gd
+++ Main.gd
@@ -54,6 +54,11 @@ func _on_MobTimer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	
+	var move_dir = randi() % 4
+	var move_x = 1 if move_dir == 0 else (-1 if move_dir == 1 else 0)
+	var move_y = 1 if move_dir == 2 else (-1 if move_dir == 3 else 0)
+	$Player.move($Player.position, $Player.position + Vector2(150.0 * move_x, 150.0 * move_y))
 
 
 func _on_ScoreTimer_timeout():
--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 var target_pos = Vector2(0.0, 0.0)
+var distance_from_start = 10.0
+
+var approaching_threshold = 5.0
 
 func _ready():
 	target_pos = position
@@ -11,13 +14,20 @@ func _process(delta):
 	var pos_diff = (target_pos - position)
 	var distance = pos_diff.length()
 	
-	if distance < 0.1:
+	if distance < approaching_threshold:
 		return
 	
 	var velocity = pos_diff / distance
 	
 	rotation = velocity.angle()
 	position += velocity * delta * 100
+
+
+func move(from, to):
+	var pos_diff = to - from
+	if from.distance_to(position) > approaching_threshold or abs(from.angle_to_point(to) - position.angle_to_point(target_pos)) > 45:
+		position = from + pos_diff.normalize() * distance_from_start
+	target_pos = to
 
 
 func start(pos):
